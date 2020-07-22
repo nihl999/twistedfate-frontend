92e7126aa8dbde0bc79e60dcf3e2c80c702aefda
Arthur Lorhan <arthurlohransilva@gmail.com>
2020-07-22 15:57:26 -0300

    Add api requests, point and local data persistance

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -4,13 +4,32 @@ import './style.css'
 class Card extends Component {
     state = { 
         responses: '',
+        correct: false,
     }
-
+    componentDidMount(){
+        this.setState({correct: this.props.correct})
+    }
+    answer = (event) => {
+        event.preventDefault()
+        if(this.state.correct === true){
+            this.props.setState({points: this.props.points+1})
+            localStorage.setItem('@tf-game/points', this.props.points+1)
+            this.props.reload()
+        }else{
+            if(this.props.points !== 0){
+                this.props.setState({points: this.props.points-1})
+                localStorage.setItem('@tf-game/points', this.props.points-1)
+                this.props.reload()
+            } else {
+                window.alert('You Lose')
+            }
+        }
+    }
     render() {
         return (
             <div className="card">
                 <h3>{this.props.response}</h3>
-                <button id="accept-card">SUBMETER</button>
+                <button id="accept-card" onClick={this.answer}>SUBMETER</button>
             </div>
         );
     }
--- src/components/game/index.js
+++ src/components/game/index.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Card from '../card'
 import './style.css'
 
 class Game extends Component {
     state = { 
         question: '',
-        responses: ['Teste1', 'Teste2', 'Teste3', 'Teste4']
+        responses: [],
+        points: 0,
+        lastQuestion: '',
     }
+    componentWillMount(){
+        if(isNaN(localStorage.getItem('@tf-game/points')))
+            this.setState({points: 0})
+        else
+            this.setState({points: parseInt(localStorage.getItem('@tf-game/points'))})
+    }
+    componentDidMount(){
+        this.searchQuestions();
+    }
+    get_random = function (list) {
+        const data = list[Math.floor((Math.random()*list.length))]
+        if(data._id !== this.state.lastQuestion){
+            this.setState({lastQuestion: data._id})
+            return data
+        } else {  
+            return this.get_random(list)
+        } 
+        //return list[Math.floor((Math.random()*list.length))];
+    } 
+    searchQuestions = async () => {
+        const { data } = await axios.get('http://localhost:4500/biologia/cards/')
+        const {question, responses} = this.get_random(data)
+        console.log(question, responses)
+        this.setState({question})
+        this.setState({responses})
+    }
+
+
     render() {
         return (
             <div id="game">
-                <div className="question-holder">Template Question</div>
+                <div className="question-holder">{this.state.question}</div>
+                <div id="points-holder"><h2>Pontos: {this.state.points}</h2></div>
                 <div className="card-holder">
-                    {this.state.responses.map(response => <Card response={response}/>)}
+                    {this.state.responses.map((response, i) => <Card 
+                    response={response.response} 
+                    key={i}
+                    correct={response.correct}
+                    setState={p=>{this.setState(p)}}
+                    points={this.state.points}
+                    reload={this.searchQuestions}
+                    />)}
                 </div>
             </div>
         );
